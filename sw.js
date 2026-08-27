af0448fddfc31daf4a6a4a598add6bf049b99b9d
naru-take <mdgfnjytb9@privaterelay.appleid.com>
2026-08-28 08:12:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,23 @@
-const CACHE="ietore-slim-v14";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+// v15: Service Worker intentionally disabled.
+// This worker removes old caches and unregisters itself.
 
-self.addEventListener("install",event=>{
+self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
 });
 
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",event=>{
-  if(event.request.mode==="navigate"){
-    event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
-          return response;
-        })
-        .catch(()=>caches.match("./index.html"))
-    );
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    try {
+      const keys = await caches.keys();
+      await Promise.all(keys.map(k => caches.delete(k)));
+      await self.registration.unregister();
+      const clientsList = await self.clients.matchAll({
+        type: "window",
+        includeUncontrolled: true
+      });
+      for (const client of clientsList) {
+        client.navigate(client.url);
+      }
+    } catch (e) {}
+  })());
 });
