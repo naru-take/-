cbad5aef2eed3a47dd69162ec411f5487e02ffa4
naru-take <mdgfnjytb9@privaterelay.appleid.com>
2026-08-27 23:00:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ietore-slim-v7";
+const CACHE="ietore-slim-v8";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
