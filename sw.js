c8af6900f82cf23c264e77d30182fc329af366b7
naru-take <mdgfnjytb9@privaterelay.appleid.com>
2026-08-29 00:05:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// v20: persistent caching disabled
+// v21: persistent caching disabled
 self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.registration.unregister()));
