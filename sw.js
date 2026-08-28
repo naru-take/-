155fbe59984f48173d852cf232760559d49b2740
naru-take <mdgfnjytb9@privaterelay.appleid.com>
2026-08-28 09:17:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// v16 - no persistent service worker
+// v17 - no persistent service worker
 self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",event=>event.waitUntil((async()=>{try{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();}catch(e){}})()));
